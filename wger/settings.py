cd27fe192b8b2c42a04cfceb0c8700b611aade9e
dmaish <mainadaniel81@gmail.com>
2018-08-10 13:38:19 +0300

    reset setting.py to original state

--- wger/settings.py
+++ wger/settings.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from wger.settings_global import *
+import os
+from wger.settings_global import *  # noqa
+import dj_database_url
 
 # Use 'DEBUG = True' to get more details for server errors
 DEBUG = True
-TEMPLATES[0]['OPTIONS']['debug'] = True
+TEMPLATES[0]['OPTIONS']['debug'] = True  # noqa
 
 ADMINS = (
     ('Your name', 'your_email@example.com'),
 )
 MANAGERS = ADMINS
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/danielmaina/Desktop/wger-app/wg-june-bunnies/wger/database.sqlite',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT")
     }
 }
+if os.environ.get("TRIGGER") == "True":
+    DATABASES["default"] = dj_database_url.config()
+
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'p8a6_)bgv-75ds-6z)+0yds_j=%*2!f7&j9tp7fkom=+hn_+%g'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Your reCaptcha keys
 RECAPTCHA_PUBLIC_KEY = ''
@@ -35,25 +38,25 @@ NOCAPTCHA = True
 # The site's URL (e.g. http://www.my-local-gym.com or http://localhost:8000)
 # This is needed for uploaded files and images (exercise images, etc.) to be
 # properly served.
-SITE_URL = 'http://localhost:8000'
+SITE_URL = os.environ.get("SITE_URL")
 
 # Path to uploaded files
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = '/Users/danielmaina/.local/share/wger/media'
+MEDIA_ROOT = '/wger/media'
 MEDIA_URL = '/media/'
 
 # Allow all hosts to access the application. Change if used in production.
 ALLOWED_HOSTS = '*'
 
 # This might be a good idea if you setup memcached
-#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 # Configure a real backend in production
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Sender address used for sent emails
-WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
+WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'  # noqa
 
 # Your twitter handle, if you have one for this instance.
-#WGER_SETTINGS['TWITTER'] = ''
+# WGER_SETTINGS['TWITTER'] = ''
