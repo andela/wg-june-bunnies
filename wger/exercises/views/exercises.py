658bb1843ded3335bae50a572d03973494d49277
Loice Meyo <loicemeyo@gmail.com>
2018-08-23 10:32:25 +0300

    Edit author's field to be a foreign field

--- wger/exercises/views/exercises.py
+++ wger/exercises/views/exercises.py
@@ -60,6 +60,7 @@ from wger.utils.widgets import (
 )
 from wger.config.models import LanguageConfig
 from wger.weight.helpers import process_log_entries
+from django.contrib.auth.models import User
 
 
 logger = logging.getLogger(__name__)
@@ -207,6 +208,10 @@ class ExercisesEditAddView(WgerFormMixin):
             muscles_secondary = ModelMultipleChoiceField(queryset=Muscle.objects.all(),
                                                          widget=TranslatedOriginalSelectMultiple(),
                                                          required=False)
+            license_author = ModelChoiceField(queryset=User.objects.all(),
+                                              widget=TranslatedSelect(),
+                                              required=False,
+                                              empty_label=None)                                           
 
             class Meta:
                 model = Exercise
@@ -217,8 +222,8 @@ class ExercisesEditAddView(WgerFormMixin):
                           'muscles',
                           'muscles_secondary',
                           'equipment',
-                          'license',
-                          'license_author']
+                          'license'
+                          ]
 
             class Media:
                 js = ('/static/bower_components/tinymce/tinymce.min.js',)
