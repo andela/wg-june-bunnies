d29c1b2eb5f5c70d291eb2c4862bdf71f3df35fe
Kyalo M <28805113+jmutuku95@users.noreply.github.com>
2018-08-23 14:22:54 +0300

    Update stepform

--- wger/manager/views/schedule_step.py
+++ wger/manager/views/schedule_step.py
@@ -18,6 +18,7 @@ import logging
 
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy, ugettext as _
 from django.db import models
 from django.forms import ModelForm, ModelChoiceField
@@ -61,12 +62,44 @@ class StepCreateView(WgerFormMixin, CreateView, PermissionRequiredMixin):
         class StepForm(ModelForm):
             workout = ModelChoiceField(
                 queryset=Workout.objects.filter(user=self.request.user))
+            
+            def __init__(self, schedule_id, *args, **kwargs):
+                super(StepForm, self).__init__(*args, **kwargs)
+                # set the schedule_id as an attribute of the form
+                self.schedule_id = schedule_id
+            
+            def clean_duration(self):
+                """Ensures duration does not exceed period"""
+                duration = self.cleaned_data['duration']
+                schedule = Schedule.objects.get(pk=self.schedule_id)
+                sum_duration = schedule.schedulestep_set.all().aggregate(models.Sum('duration'))
+                # Catch NoneType error for 1st workout to be added to schedule.
+                sum_duration = sum_duration['duration__sum'] or 0
+                derived_duration = duration + sum_duration
+    
+                # Check if duration exceeds cycle duration
+                if schedule.period == 'Macrocycle' and derived_duration > 52:
+                    msg = 'Duration Cycle exceeds 1 year. \'{}\' weeks are left in the cycle'.format(52 - sum_duration)
+                    raise ValidationError(_(msg))
+                elif schedule.period == 'Mesocycle' and derived_duration > 6:
+                    msg = 'Duration cycle excedds 6 weeks. \'{}\' weeks are left in the cycle'.format(6-sum_duration)
+                    raise ValidationError(_(msg))
+                elif schedule.period == 'Microcycle' and derived_duration > 1:
+                    raise ValidationError(_(
+                        'Workout already exists. Delete existing workout to add new one'))
+                return duration
 
             class Meta:
                 model = ScheduleStep
                 exclude = ('order', 'schedule')
 
+
         return StepForm
+    
+    def get_form_kwargs(self):
+        kwargs = super(CreateView, self).get_form_kwargs()
+        kwargs['schedule_id'] = self.kwargs['schedule_pk']
+        return kwargs
 
     def get_context_data(self, **kwargs):
         context = super(StepCreateView, self).get_context_data(**kwargs)
